a22de675f3781bdd9e6c391998eaf8e6f8fc214b
Justin Foutts <jmfoutts@gmail.com>
2019-02-25 19:54:20 -0800

    Add load and save image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
+import 'dart:io';
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:simple_permissions/simple_permissions.dart';
+
+const directoryName = 'Photoducer';
 
 void main() => runApp(PhotoducerApp());
 
@@ -10,67 +18,176 @@ class PhotoducerApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: PhotoducerHomePage(),
+      home: Photoducer(),
     );
   }
 }
 
-class PhotoducerHomePage extends StatefulWidget {
+class Photoducer extends StatefulWidget {
   @override
-  _PhotoducerHomePageState createState() => _PhotoducerHomePageState();
+  _PhotoducerState createState() => _PhotoducerState();
 }
 
-class _PhotoducerHomePageState extends State<PhotoducerHomePage> {
-  List<Offset> points = <Offset>[];
+class _PhotoducerState extends State<Photoducer> {
+  GlobalKey<_PhotoducerCanvasState> canvasKey = GlobalKey();
+  Permission readPermission = Permission.ReadExternalStorage;
+  Permission writePermission = Permission.WriteExternalStorage;
+  var renderedImage;
 
   @override
   Widget build(BuildContext context) {
-    final Container sketchContainer = Container(
-      margin: EdgeInsets.all(1.0),
-      alignment: Alignment.topLeft,
-      color: Colors.blueGrey[50],
-      child: CustomPaint(
-        painter: Painter(points),
-      ),
-    );
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Photoducer'),
       ),
-      body: GestureDetector(
+
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: PhotoducerCanvas(canvasKey),
+          ),
+          Container(
+            height: 100,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                FlatButton(
+                  child: Icon(Icons.save),
+                  onPressed: () { 
+                    setState(() {
+                       renderedImage = canvasKey.currentState.rendered;
+                    });
+                    saveImage(context);
+                  },
+                ),
+
+                FlatButton(
+                  child: Icon(Icons.open_in_browser),
+                  onPressed: () { 
+                    loadImage(context);
+                  },
+                ),
+
+                FlatButton(
+                  child: Icon(Icons.refresh),
+                  onPressed: () {
+                    canvasKey.currentState.clear();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<Null> loadImage(BuildContext context) async {
+    if(!(await checkPermission(readPermission))) await requestPermission(readPermission);
+    Directory directory = await getExternalStorageDirectory();
+    String path = directory.path;
+    ui.Codec codec = await ui.instantiateImageCodec(File('$path/$directoryName/photo.png').readAsBytesSync());
+    ui.FrameInfo frame = await codec.getNextFrame();
+    canvasKey.currentState.setBackgroundImage(frame.image);
+  }
+
+  Future<Null> saveImage(BuildContext context) async {
+    var pngBytes = await renderedImage.toByteData(format: ui.ImageByteFormat.png);
+    if(!(await checkPermission(writePermission))) await requestPermission(writePermission);
+    Directory directory = await getExternalStorageDirectory();
+    String path = directory.path;
+    debugPrint('Creating ' + path + '/' + directoryName);
+    await Directory('$path/$directoryName').create(recursive: true);
+    File('$path/$directoryName/photo.png').writeAsBytesSync(pngBytes.buffer.asInt8List());
+  }
+
+  requestPermission(permission) async {
+    PermissionStatus status = await SimplePermissions.requestPermission(permission);
+    return status == PermissionStatus.authorized;
+  }
+
+  checkPermission(permission) async {
+    bool result = await SimplePermissions.checkPermission(permission);
+    return result;
+  }
+}
+
+class PhotoducerCanvas extends StatefulWidget {
+  PhotoducerCanvas(Key key): super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _PhotoducerCanvasState();
+  }
+}
+
+class _PhotoducerCanvasState extends State<PhotoducerCanvas> {
+  ui.Image backgroundImage;
+  List<Offset> points = <Offset>[];
+
+  void clear() {
+    setState(() {
+      points.clear();
+      backgroundImage = null;
+    });
+  }
+
+  void setBackgroundImage(ui.Image image) {
+    setState(() {
+      points.clear();
+      backgroundImage = image;
+    });
+  }
+
+  ui.Image get rendered {
+    ui.PictureRecorder recorder = ui.PictureRecorder();
+    Canvas canvas = Canvas(recorder);
+    PhotoducerCanvasPainter painter = PhotoducerCanvasPainter(backgroundImage, points);
+    var size = context.size;
+    painter.paint(canvas, size);
+    return recorder.endRecording().toImage(size.width.floor(), size.height.floor());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(1.0),
+      child: GestureDetector(
         onPanUpdate: (DragUpdateDetails details) {
           setState(() {
             RenderBox box = context.findRenderObject();
             Offset point = box.globalToLocal(details.globalPosition);
-            point = point.translate(0.0, -(AppBar().preferredSize.height));
-            points = List.from(points)..add(point);
+            if (point.dy < box.size.height)
+              points = List.from(points)..add(point);
           });
         },
+
         onPanEnd: (DragEndDetails details) {
-          points.add(null);
+          setState(() {
+            points.add(null);
+          });
         },
-        child: sketchContainer,
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'clear Screen',
-        backgroundColor: Colors.red,
-        child: Icon(Icons.refresh),
-        onPressed: () {
-          setState(() => points.clear());
-        },
+
+        child: Container(
+          alignment: Alignment.topLeft,
+          color: Colors.blueGrey[50],
+          child: CustomPaint(
+            painter: PhotoducerCanvasPainter(backgroundImage, points),
+          ),
+        ),
       ),
     );
   }
 }
 
-class Painter extends CustomPainter {
+class PhotoducerCanvasPainter extends CustomPainter {
+  final ui.Image backgroundImage;
   final List<Offset> points;
 
-  Painter(this.points);
+  PhotoducerCanvasPainter(this.backgroundImage, this.points);
 
   @override
-  bool shouldRepaint(Painter oldDelegate) {
+  bool shouldRepaint(PhotoducerCanvasPainter oldDelegate) {
     return oldDelegate.points != points;
   }
 
@@ -79,6 +196,10 @@ class Painter extends CustomPainter {
       ..color = Colors.black
       ..strokeCap = StrokeCap.round
       ..strokeWidth = 4.0;
+
+    if (backgroundImage != null) {
+      canvas.drawImage(backgroundImage, Offset(0, 0), paint);
+    }
 
     for (int i = 0; i < points.length - 1; i++) {
       if (points[i] != null && points[i + 1] != null) {
